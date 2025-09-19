a6ab9bedea48a5b8913bb693cc6327ed5094069f
Ulil Fahmi <ulilf442@gmail.com>
2025-09-19 20:13:58 +0700

    branch bmi calculation

--- main.dart
+++ main.dart
@@ -1,15 +1,35 @@
 void main() {
   List<Map<String, dynamic>> riwayat = [];
 
-  hitungBMI(170, 65, riwayat);  // valid
-  hitungBMI(-165, 75, riwayat); // tidak valid
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(160, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+  
 }
-
 void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   if (tinggiCm <= 0 || beratKg <= 0) {
-    print("Input tidak valid! Tinggi dan berat harus lebih dari 0.");
+    print("Input tidak valid!");
     return;
   }
 
-  print("Input valid: Tinggi = $tinggiCm cm, Berat = $beratKg kg");
+    // Konversi tinggi dari cm ke m
+  double tinggiM = tinggiCm / 100;
+
+  // Hitung BMI
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+    // Tentukan kategori
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
+    print("Tinggi: $tinggiCm cm | Berat: $beratKg kg | "
+      "BMI: ${bmi.toStringAsFixed(2)} | Kategori: $kategori");
 }
