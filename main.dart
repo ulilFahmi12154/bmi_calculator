cff19399faaf989cb027bfb3f3135c2b45f5bdbf
Ulil Fahmi <ulilf442@gmail.com>
2025-09-19 20:21:43 +0700

    branch history feature

--- main.dart
+++ main.dart
@@ -1,25 +1,25 @@
 void main() {
   List<Map<String, dynamic>> riwayat = [];
 
+  // Simulasi beberapa input
   hitungBMI(170, 65, riwayat);
-  hitungBMI(160, 75, riwayat);
+  hitungBMI(165, 75, riwayat);
   hitungBMI(180, 85, riwayat);
-  
+
+  // Tampilkan riwayat
+  tampilkanRiwayat(riwayat);
 }
+
 void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
   if (tinggiCm <= 0 || beratKg <= 0) {
     print("Input tidak valid!");
     return;
   }
 
-    // Konversi tinggi dari cm ke m
   double tinggiM = tinggiCm / 100;
-
-  // Hitung BMI
   double bmi = beratKg / (tinggiM * tinggiM);
 
-    // Tentukan kategori
-  String kategori;
+    String kategori;
   if (bmi < 18.5) {
     kategori = "Kurus";
   } else if (bmi < 25) {
@@ -30,6 +30,18 @@ void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riway
     kategori = "Obesitas";
   }
 
-    print("Tinggi: $tinggiCm cm | Berat: $beratKg kg | "
-      "BMI: ${bmi.toStringAsFixed(2)} | Kategori: $kategori");
+    riwayat.add({
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi.toStringAsFixed(2),
+    'kategori': kategori,
+  });
+}
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  print("\n=== Riwayat Perhitungan BMI ===");
+  for (var data in riwayat) {
+    print("Tinggi: ${data['tinggi']} cm | Berat: ${data['berat']} kg | "
+        "BMI: ${data['bmi']} | Kategori: ${data['kategori']}");
+  }
 }
