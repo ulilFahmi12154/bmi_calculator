38eb7a130a8ffb48b7158494dd88a0c90f75b71f
Ulil Fahmi <ulilf442@gmail.com>
2025-09-19 19:52:35 +0700

    branch input validation

--- main.dart
+++ main.dart
@@ -1,3 +1,15 @@
 void main() {
-  print("BMI Calculator Project - Baseline");
+  List<Map<String, dynamic>> riwayat = [];
+
+  hitungBMI(170, 65, riwayat);  // valid
+  hitungBMI(-165, 75, riwayat); // tidak valid
+}
+
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
+  if (tinggiCm <= 0 || beratKg <= 0) {
+    print("Input tidak valid! Tinggi dan berat harus lebih dari 0.");
+    return;
+  }
+
+  print("Input valid: Tinggi = $tinggiCm cm, Berat = $beratKg kg");
 }
